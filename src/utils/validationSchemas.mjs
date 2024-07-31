5f2a765155ad57bfa39480fcb7617dcc1768c032
koyu1111 <np05cp4s230006@iic.edu.np>
2024-07-31 18:11:21 +0545

    06:11

---
+++ src/utils/validationSchemas.mjs
@@ -0,0 +1,21 @@
+export const creatUserValidationSchema = {
+    username:{
+        isLength: {
+            options:{
+                min: 5,
+                max: 32,
+            },
+            errorMessage:
+            "Username must be at least 5 characters with a max of 32 character",        
+        },
+        notEmpty: {
+            errorMessage: "Username cannot be empty",
+        },
+        isString: {
+            errorMessage: " Username must be string"
+        },        
+    },
+    displayName: {
+        notEmpty:true,
+    }
+};
