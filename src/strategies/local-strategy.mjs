d16ea12e5e0d322baca683551884271585f4ee52
koyu1111 <np05cp4s230006@iic.edu.np>
2024-08-01 19:00:25 +0545

    password-error solved in status

--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -10,18 +10,29 @@ passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
+// passport.deserializeUser((id, done) => {
+//     console.log(`Inside Deserialize User`);
+//     console.log(`Deserializing User ID: ${id}`);
+//     try{
+//         const findUser = mockUsers.find((user) => user.id === id);
+//         if(!findUser) throw new Error("User Not Found");
+//         done(null, findUser);
+//     } catch (err) {
+//         done(err, null);
+//     };
+// });
+
 passport.deserializeUser((id, done) => {
     console.log(`Inside Deserialize User`);
     console.log(`Deserializing User ID: ${id}`);
-    try{
-        const findUser = mockUsers.find((user) => user.id === id);
-        if(!findUser) throw new Error("User Not Found");
-        done(null, findUser);
+    try {
+      const findUser = mockUsers.find((user) => user.id === id);
+      if (!findUser) throw new Error("User Not Found");
+      done(null, findUser);
     } catch (err) {
-        done(err, null);
-    };
-});
-
+      done(err, null);
+    }
+  });
 export default passport.use(
     new Strategy((username, password, done) => {
         console.log(`Username: ${username}`);
@@ -30,7 +41,7 @@ export default passport.use(
         try {
             const findUser = mockUsers.find((user) => user.username === username );
         if (!findUser) throw new Error("User not found");
-        if ( findUser.password !== password)
+        if ( findUser.password !== password)  
             throw new Error("Invalid Credentials");
         done(null, findUser);
         } catch (err) {
